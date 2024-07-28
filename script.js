c809e6c5e99840a9bbbd95fc062e0aa3734c9dc4
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-07-28 11:28:18 +0100

    Added Delete and AC Buttons

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".num");
+const deleteBtns = document.querySelectorAll(".top>button")
 
 numBtns.forEach((number)=>{
     number.addEventListener(("click"),()=>{
         display.innerHTML += number.textContent;
     })
+})
+
+deleteBtns[0].addEventListener("click",()=>{
+    display.innerHTML = "";
+})
+
+deleteBtns[1].addEventListener("click",()=>{
+    display.innerHTML =display.textContent.slice(0, -1);
 })
