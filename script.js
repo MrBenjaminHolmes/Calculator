f458463c7be7ca6b5234abee63fc87478f212845
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-07-28 12:28:35 +0100

    Added Operator Selectors

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const display = document.querySelector(".display");
 const numBtns = document.querySelectorAll(".num");
-const deleteBtns = document.querySelectorAll(".top>button")
+let operatorSelected = false;
+let value1= "";
+let operator = ""
+let value2 ="";
+const equals = document.querySelector("#equals");
+const operators = document.querySelectorAll(".operator");
+const deleteBtns = document.querySelectorAll(".top>button");
+
+function removeSelected(){
+    operators.forEach((operatorBtn)=>{
+        operatorBtn.classList.remove("selected");
+    })
+}
 
 numBtns.forEach((number)=>{
     number.addEventListener(("click"),()=>{
-        display.innerHTML += number.textContent;
+        if (operatorSelected === true){
+            display.innerHTML = "";
+            display.innerHTML += number.textContent;
+            operatorSelected = false;
+
+        }
+        else
+        {
+            display.innerHTML += number.textContent;
+        }
+        
     })
 })
 
@@ -15,3 +37,17 @@ deleteBtns[0].addEventListener("click",()=>{
 deleteBtns[1].addEventListener("click",()=>{
     display.innerHTML =display.textContent.slice(0, -1);
 })
+
+operators.forEach((operatorBtn)=>{
+    operatorBtn.addEventListener("click",()=>{
+        console.log(operatorBtn)
+        value1 = display.textContent
+        operator = operatorBtn.textContent
+        operatorSelected = true;
+        removeSelected()
+        operatorBtn.classList.add("selected")
+        
+    })
+
+})
+
